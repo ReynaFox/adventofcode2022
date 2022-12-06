bb9cf27471f7bef85937263c38c71569d3e380e9
Reyna <reynafox@zohomail.eu>
2022-12-06 19:09:16 +0100

    Day 6.

---
+++ Day6/Program.cs
@@ -0,0 +1,13 @@
+﻿part1(4);
+part1(14); // Part 2
+
+static void part1(int length) {
+	foreach (var line in File.ReadLines("input.txt")) {
+		for (int i = 0; i < line.Length-length; i++) {
+			if (new HashSet<char>(line[i..(i+length)]).Count == length) {
+				Console.WriteLine(i+length);
+				break;
+			}
+		}
+	}
+}
